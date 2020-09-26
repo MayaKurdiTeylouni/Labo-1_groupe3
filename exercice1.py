cefdbbb66d2ff07336169015cb8609d21fc2721c
MayaKurdiTeylouni <71103551+MayaKurdiTeylouni@users.noreply.github.com>
2020-09-26 17:33:42 -0400

    Initialize cicero-mt-patch-1

---
+++ exercice1.py
@@ -0,0 +1,13 @@
+def calculerEnergie(masse, vitesse):
+
+    # TODO convertir la vitesse en metre par seconde, assigner la valeur à la variable "vitesse"
+    vitesse=
+
+    # TODO calculer l'energie cinetique, assigner la valeur à la variable "energieCinetique"
+    energieCinetique =
+    return energieCinetique
+
+if __name__ == '__main__':
+    masse = float(input("indiquez la masse(en kg) de la voiture: "))
+    vitesse = float(input("indiquez la vitesse (en km/h) de la voiture: "))
+    calculerEnergie(masse, vitesse)
---
+++ exercice3.py
@@ -0,0 +1,16 @@
+import math
+
+
+def calculerNombreChiffres(nombre):
+
+    # TODO: Déterminer le nombre de chiffres de "nombre" et mettre la valeur dans "nombreDeChiffres"
+    nombreDeChiffres =
+
+    # TODO: Afficher la valeur de "nombreDeChiffres"
+
+    return nombreDeChiffres
+
+
+if __name__ == '__main__':
+    nombre = int(input('veuillez indiquer un nombre strictement postif: '))
+    calculerNombreChiffres(nombre)
